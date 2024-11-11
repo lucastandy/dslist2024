977c85e5fb33fcece28aeb9ff3e187cb6d030f14
Lucas Tandy <lucastitandy@gmail.com>
2024-11-11 13:46:04 -0300

    Project created

---
+++ src/main/java/com/devsuperior2024/dslist2024/Dslist2024Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior2024.dslist2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior2024/dslist2024/Dslist2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior2024.dslist2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
