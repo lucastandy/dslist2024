89f3af6d4cb4245c1a4a3bae02954e2fe56a739f
lucastandy <lucastitandy@gmail.com>
2024-11-11 20:01:08 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/devsuperior2024/dslist2024/entities/Game.java
@@ -0,0 +1,133 @@
+package com.devsuperior2024.dslist2024.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity // Essa notation vai cofigurar esta classe Java a uma tabela no banco de dados.
+@Table(name = "tb_game") //Configurando o nome da tabela
+public class Game {
+	
+	@Id // Configurando o atributo id para o banco de dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Configurando o campo para ser autoincrementado
+	private Long id;
+	private String title;
+	@Column(name = "game_year") // Customizando o nome do atributo no banco de dados
+	private Integer year;
+	private String genre;
+	private String platform;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+	/*Método construtor padrão vazio*/
+	public Game() {
+		
+	}
+
+	/*Método construtor padrão com atributos.*/
+	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+			String shortDescription, String longDescription) {
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+		this.platform = platform;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+	}
+
+	/*Métodos Getters e Setters*/
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	
+	/*Serve para comparar os objetos entre si.*/
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
